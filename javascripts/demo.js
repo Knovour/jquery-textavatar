a3af9b0eb06c945dbaa87aa6338fd24aa068054b
Knovour Zheng <knovourimba@gmail.com>
2013-12-29 21:16:03 +0800

    page update

---
+++ javascripts/demo.js
@@ -0,0 +1,54 @@
+$('.textavatar').textAvatar();
+$('#scroll-spy').niceScroll({
+  boxzoom: true,
+  horizrailenabled: false,
+  cursorwidth: '8'
+});
+var $content = $('#test');
+
+urlSwitch(location.hash);
+
+$(window).on('hashchange', function(e) {urlSwitch(location.hash);});
+
+$('input').on('keyup keydown keypress change', function() {
+  var name = $('#name').val();
+  var size = $('#size').val();
+  
+  if(!name) {
+    $content.hide();
+    return;
+  }
+  
+  $content.show().textAvatar({
+    name: name,
+    width: size
+  });
+});
+
+$('#main-nav a').click(function(e) {
+  e.preventDefault();
+  switchTo(this.hash);
+  tabSwitch($(this));
+});
+
+function urlSwitch(idFromUrl) {
+  if($(idFromUrl).length)
+    tabSwitch($('a[href=' + idFromUrl + ']'));
+}
+
+function switchTo(hash) {
+  console.log(hash);
+  $('#scroll-spy').animate({
+    scrollTop: $(hash).offset().top
+  }, 300, function(){
+  
+    // when done, add hash to url
+    // (default click behaviour)
+    window.location.hash = hash;
+  });
+}
+
+function tabSwitch($a) {
+  $('#main-nav li.active').removeClass('active');
+  $a.closest('li').addClass('active');
+}
---
+++ javascripts/jquery.textavatar.js
@@ -0,0 +1,51 @@
+// Text Avatar v0.0.1, 2013/10/14, Knovour Zheng
+(function ($) {
+	'use strict';
+
+	var _oneWord = 26; //26px
+	$.fn.textAvatar = function(options) {
+		var _abbrTemplate = '<abbr title="_name_">_text_avatar_</abbr>';
+		var _reg = /[\u4E00-\u9FA5\uF900-\uFA2D]/i; //check Chinese, Japenese & Korean character
+
+		var _contentLength = this.length;
+		for(var i = 0; i < _contentLength; i++) {
+			// tAvatar => textAvatar
+			var _tAvatarContent = this.eq(i);
+			var _defaultOptions = {
+				name: (!_tAvatarContent.data('name')) ? '' : _tAvatarContent.data('name').trim(),
+				width: _tAvatarContent.width(),
+			};
+
+			$.extend(_defaultOptions, options);
+
+			var _tAvatar = '';
+			if(!_reg.test(_defaultOptions.name)) {
+				//(/ +/g, ' '): replace multi space to one space, prevent typing error
+				var text = _defaultOptions.name.replace(/ +/g, ' ').split(' ');
+				var length = (text.length > 2) ? 2 : text.length;
+				for(var j = 0; j < length; j++) {
+					var temp = text[j].trim()[0];
+					_tAvatar += (temp !== undefined) ? temp.toUpperCase() : '';
+				}
+			}
+
+			else
+				_tAvatar = _defaultOptions.name[0];
+
+			_tAvatar = (_defaultOptions.width <= _oneWord) ? _tAvatar[0] : _tAvatar;
+
+			var _newAbbr = _abbrTemplate
+				.replace(/_name_/i, _defaultOptions.name)
+				.replace(/_text_avatar_/i, _tAvatar);
+
+			var _avatarWidth = _defaultOptions.width;
+			_tAvatarContent.html(_newAbbr).css({
+				width: _avatarWidth,
+				height: _avatarWidth,
+				'font-size': _getFontSize(_avatarWidth)
+			});
+		}
+	};
+
+	function _getFontSize(width) {return (width * 0.4 + 'px');}
+})(jQuery);
